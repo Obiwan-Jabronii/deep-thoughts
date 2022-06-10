a16e930d34418f7e9cc8c3e3e15ffbe645f36110
Obi Wan Jabronii <mitchell_lough@yahoo.com>
2022-06-10 12:48:12 -0400

    added routes for the profiles, thoughts, reactions and friends list

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+//importing the pages 
+import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+
+//import the components
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-import Home from './pages/Home';
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-
+//creates the http link 
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
@@ -15,16 +24,44 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
+//builds the app bringing together the pages and components
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className='flex-column justify-flex-start min-100-vh'>
-        <Header />
-        <div className='container'>
-          <Home />
+      <Router>
+        <div className='flex-column justify-flex-start min-100-vh'>
+          <Header />
+          <div className='container'>
+            <Routes>
+              <Route
+                path="/"
+                element={<Home />}
+                />
+              <Route 
+                path="/login"
+                element={<Login />}
+              />
+              <Route
+                path="/signup"
+                element={<Signup />}
+              />
+              <Route
+                path="/profile/:username"
+                element={<Profile />}
+              />
+              <Route
+                path="/thought/:id"
+                element={<SingleThought />}
+              />
+              <Route
+                path="*"
+                element={<NoMatch />}
+              />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
